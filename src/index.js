0fc0d9dc43dbe92e41102185fdd705fa38c9db1e
Hrutav Modha <modhahrutav@gmail.com>
2025-05-22 10:51:44 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
 div = document.getElementById("root")
-root = React.createRoot(div)
+root = ReactDOM.createRoot(div)
 root.render(<App/>)
